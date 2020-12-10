8acda27245b81477b3b5704d8a0fe4fe2954c3e5
domburi sukiyano <domburi0211@gmail.com>
2020-12-10 11:24:09 +0900

    ピンボール部分の実装 とりあえずマウスでの操作から跳ね返り処理まで

---
+++ Assets/Scripts/PlayerMotion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour
+{
+    [System.Serializable]  //　範囲制限
+    public class Bounds
+    {
+        public float xMin, xMax, yMin, yMax;
+    }
+
+    [SerializeField] Bounds bounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // マウス位置をスクリーン座標からワールド座標へ
+        Vector2 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // 範囲制限
+        targetPos.x = Mathf.Clamp(targetPos.x, bounds.xMin, bounds.xMax);
+        targetPos.y = Mathf.Clamp(targetPos.y, bounds.yMin, bounds.yMax);
+
+        transform.position = targetPos;
+    }
+}
