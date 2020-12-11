ed2deb87c63fe2e38dae17f3117e661f50f84fd0
domburi sukiyano <domburi0211@gmail.com>
2020-12-11 10:50:37 +0900

    アイテム穴の設置処理の実装

---
+++ Assets/Scripts/PhaseManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhaseManager : MonoBehaviour
+{
+    public bool _pinballPhase;
+    public bool _stageEditPhase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StagePanelEdit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StagePanelEdit : MonoBehaviour
+{
+    public GameObject itemSpace;
+    [Header("アイテム穴の個数及びそれぞれの座標 : x: -3 ~ 3, y: -1 ~ 1")]
+    public Vector2[] vectors;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 parent = this.transform.position;
+        foreach(Vector2 vec in vectors)
+        {
+            Instantiate(itemSpace, parent + vec, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
