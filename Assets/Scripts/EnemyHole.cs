cb6226034ab8931b6a14b72641e4c562b150c578
domburi sukiyano <domburi0211@gmail.com>
2020-12-18 15:57:41 +0900

    敵のポップ生成システム処理　実装準備

---
+++ Assets/Scripts/EnemyHole.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHole : MonoBehaviour
+{
+    float appearTime;
+    List<GameObject> enemyList;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/EnemyHoleManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.SerializableAttribute]
+public class ValueList
+{
+    public List<int> phaseList = new List<int>();
+
+
+}
+
+
+public class EnemyHoleManager : MonoBehaviour
+{
+    public List<GameObject> enemyList;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
