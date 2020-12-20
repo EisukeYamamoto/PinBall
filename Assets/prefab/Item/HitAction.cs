d3988af19261310f8227f854a4b57557cc9c6647
tomogon100 <tomogon98@gmail.com>
2020-12-20 11:12:05 +0900

    アイテム関連。Particleアセット追加

---
+++ Assets/prefab/Item/HitAction.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class HitAction : MonoBehaviour
+{
+
+    public ParticleSystem Particle;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Instantiate(Particle,transform.position,Quaternion.identity);
+        transform.DOPunchScale(new Vector3(0.2f, 0.2f, 0.2f), 0.1f, 10, 0);
+    }
+}
